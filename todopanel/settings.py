4a2ae40bc015b3a51db608f0a9bcc9a5d880c454
Hossein Sadeghi <sadeghi.ho@hotmail.com>
2025-02-20 18:16:28 +0330

    Remove unused dependencies to optimize the projec

--- todopanel/settings.py
+++ todopanel/settings.py
@@ -38,9 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'crispy_forms',
-    'django_filters',
     'todo',
     'accounts',
 ]
